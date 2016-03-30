da738259792ab2186485c75417f5a466a450f0c9
Nazri Ramliy <ayiehere@gmail.com>
2016-03-30 08:54:35 +0800

    initial

---
+++ logfreq-plot.sh
@@ -0,0 +1,182 @@
+#!/bin/sh
+set -e
+me=$(basename $0)
+args="$(basename $0) $@"
+
+usage() {
+	local default_format_x
+	default_format_x=$(echo "$format_x"|paste -s -d'X'|sed -e 's/X/\\\\n/g')
+	echo "Usage: $me [-h] [-t <black|white>] [-W width] [-H height] <log.txt>
+
+log.txt is the file produced by logfreq
+
+OPTIONS
+    -d <duration>
+	Limit xrange max to <duration>, e.g: -d '1 hour'
+
+    -E <end>
+	Set xrange max to <end>. Use with -S.
+
+    -f <time format>
+	Value to use as timefmt. Default is '%d/%m/%y %H:%M' (log per minute)
+
+    -h
+	Show this help message
+
+    -n
+	Dry run - show what is going to be executed.
+
+    -S <start>
+	Set xrange min to <start>. Use with -E.
+
+    -s <since>
+	Limit xrange min to <since>, e.g: -s '1 day ago'
+
+    -T <title prefix>
+
+    -t <black|white>
+	Use black or white theme. Default is white.
+
+    -W <width>
+
+    -H <height>
+
+    -x <format_x>
+        Format for x ticks, default is $default_format_x"
+}
+
+titlePrefix=""
+width=1000
+height=500
+theme=white
+since=
+format_x='%l%p\n%a\n%d'
+duration=
+dryrun=
+UNTIL=
+SINCE=
+timefmt='%Y/%m/%d %H:%M'
+while getopts d:E:f:hnW:H:S:s:T:t:x: opt
+do
+	case "$opt" in
+		d)
+			duration=$OPTARG
+			;;
+		E)
+			UNTIL=$OPTARG
+			;;
+		f)
+			timefmt=$OPTARG
+			;;
+		n)
+			dryrun=t
+			;;
+		W)
+			width=$OPTARG
+			;;
+		H)
+			height=$OPTARG
+			;;
+		h)
+			usage
+			exit
+			;;
+		S)
+			SINCE=$OPTARG
+			;;
+		s)
+			since=$OPTARG
+			;;
+		T)
+			titlePrefix="$OPTARG "
+			;;
+		t)
+			theme="$OPTARG"
+			;;
+		x)
+			format_x=$OPTARG
+			;;
+		\?)
+			echo "$me: Unknown option '$opt'"
+			exit 1
+			;;
+	esac
+done
+shift $(($OPTIND -1))
+
+if [ -z "$1" ]; then
+  usage
+  exit 1
+fi
+
+if [ "$theme" = "black" ]; then
+  fg=white
+  bg=black
+elif [ "$theme" = "white" ]; then
+  bg=white
+  fg=black
+else
+  echo "Unknown theme: $theme"
+  exit 1
+fi
+
+
+log=$1
+
+# Figure out time format
+first_time=$(head -2 "$log"|tail -1|awk '{print $2}')
+case "$first_time" in
+	*:*:*)
+		timefmt='%Y/%m/%d %H:%M:%S'
+esac
+
+#png_file=$log-$(date -Iseconds).png
+png_file=$log.png
+xmin=
+xmax=
+
+#title="${titlePrefix}Log Frequency from $(head -2 "$log"|tail -1|awk '{print $1" "$2}') - $log"
+if [ -n "$SINCE" ]; then
+	xmin="\"$SINCE\""
+	#title="${titlePrefix}Log Frequency from $SINCE - $log"
+	if [ -n "$UNTIL" ]; then
+		xmax="\"$UNTIL\""
+		#title="${titlePrefix}Log Frequency from $SINCE - $UNTIL - $log"
+	fi
+	log_start=$SINCE
+elif [ -n "$since" ]; then
+	xmin=\"$(date --utc -d "$since" +"$timefmt")\"
+	if [ -n "$duration" ]; then
+		xmax=\"$(date --utc -d "$since + $duration" +"$timefmt")\"
+	fi
+fi
+title="$args"
+
+if [ -n "$xmin" -o -n "$xmax" ]; then
+	xrange='set xrange ['$xmin':'$xmax']'
+fi
+
+gnuplot_cmd="set xdata time;
+set timefmt \"$timefmt\";
+set title '$title' textcolor rgb \"$fg\";
+set key autotitle columnhead textcolor rgb \"$fg\";
+set format x \"$format_x\";
+$xrange;
+set xlabel \"Time\" textcolor rgb \"$fg\";
+set ylabel \"Frequency\" textcolor rgb \"$fg\";
+set yrange [0:];
+set terminal png size $width,$height background rgb\"$bg\";
+set border lw 1 lc rgb \"$fg\";
+set xtics textcolor rgb \"$fg\";
+set ytics textcolor rgb \"$fg\";
+set grid linecolor rgb \"gray\";
+
+plot '$log' using 1:\"frequency\" with impulses;"
+
+if [ -n "$dryrun" ]; then
+  echo "gnuplot -persist -e \"$gnuplot_cmd\""
+else
+  gnuplot -persist -e "$gnuplot_cmd" >$png_file
+  echo qiv $png_file &&
+  qiv $png_file
+fi
---
+++ logfreq.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"sort"
+	"strings"
+	"time"
+)
+
+var firstTime time.Time
+var lastTime time.Time
+
+//var lastT, last2T *string
+var tf *string
+var delta *time.Duration
+
+var empty string = ""
+
+func main() {
+	f := flag.String("f", "", "Time format - %T %C %H %M %S %m %d %y %b."+
+		"\n\tMore precise format can be given via -re and -tf")
+	regex := flag.String("re", "", "Regex to extract date and time")
+	tf = flag.String("tf", "", "Time format")
+
+	flag.Parse()
+
+	if *f != "" {
+		*regex = "(" + *f + ")"
+		*tf = *f
+	}
+
+	if *regex == "" {
+		fmt.Fprintf(os.Stderr, "Time regex must not be empty (-re <regexp)\n")
+		os.Exit(1)
+	}
+	if *tf == "" {
+		fmt.Fprintf(os.Stderr, "Time format must not be empty (-tf <format)\n")
+		os.Exit(1)
+	}
+	tfmt := "2006/01/02 15:04"
+	if strings.Index(*tf, "%T") != -1 || strings.Index(*tf, "%S") != -1 {
+		tfmt = "2006/01/02 15:04:05"
+	}
+
+	*regex = strings.Replace(*regex, "%Z", `[A-Z][A-Z][A-Z][A-Z]?`, -1)
+	*regex = strings.Replace(*regex, "%z", `[+-]\d\d\d\d`, -1)
+	*regex = strings.Replace(*regex, "%b", `[A-Z][a-z][a-z]`, -1)
+	*regex = strings.Replace(*regex, "%T", `\d\d:\d\d:\d\d`, -1)
+	*regex = strings.Replace(*regex, "%C", `\d\d`, -1)
+	*regex = strings.Replace(*regex, "%H", `\d\d`, -1)
+	*regex = strings.Replace(*regex, "%M", `\d\d`, -1)
+	*regex = strings.Replace(*regex, "%S", `\d\d`, -1)
+	*regex = strings.Replace(*regex, "%m", `\d\d`, -1)
+	*regex = strings.Replace(*regex, "%d", `\d\d`, -1)
+	*regex = strings.Replace(*regex, "%Y", `\d\d\d\d`, -1)
+
+	*tf = strings.Replace(*tf, "%Z", "MST", -1)
+	*tf = strings.Replace(*tf, "%z", "-0700", -1)
+	*tf = strings.Replace(*tf, "%b", "Jan", -1)
+	*tf = strings.Replace(*tf, "%T", "15:04:05", -1)
+	*tf = strings.Replace(*tf, "%C", `06`, -1)
+	*tf = strings.Replace(*tf, "%H", `15`, -1)
+	*tf = strings.Replace(*tf, "%M", `04`, -1)
+	*tf = strings.Replace(*tf, "%S", `05`, -1)
+	*tf = strings.Replace(*tf, "%m", `01`, -1)
+	*tf = strings.Replace(*tf, "%d", `02`, -1)
+	*tf = strings.Replace(*tf, "%Y", `2006`, -1)
+
+	re := regexp.MustCompile(*regex)
+
+	ret := 0
+	files := []string{"-"}
+	if len(flag.Args()) > 0 {
+		files = flag.Args()
+	}
+
+	var src io.Reader
+
+	freq := map[string]int{}
+	for _, file := range files {
+		if file == "-" {
+			src = os.Stdin
+		} else {
+			f, err := os.Open(file)
+			if err != nil {
+				log.Println(err)
+				ret = 1
+				continue
+			}
+			src = f
+		}
+
+		lnum := 0
+		r := bufio.NewScanner(src)
+		for r.Scan() {
+			line := r.Text()
+			lnum++
+			m := re.FindStringSubmatch(line)
+			if m == nil {
+				continue
+			}
+			tstamp := m[1]
+			t, err := time.Parse(*tf, tstamp)
+			if err != nil {
+				log.Fatal(err)
+			}
+			normalizedTstamp := t.Format(tfmt)
+			//fmt.Fprintf(os.Stderr, "tstamp %v => %v\n", tstamp, normalizedTstamp)
+			freq[normalizedTstamp] = freq[normalizedTstamp] + 1
+		}
+	}
+	keys := []string{}
+	for k, _ := range freq {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	fmt.Println("date time frequency")
+	for _, k := range keys {
+		fmt.Println(k, freq[k])
+	}
+	os.Exit(ret)
+}
